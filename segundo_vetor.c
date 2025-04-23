7a814d9dd0c2aef973f5ab30afc92ba7f997a9fc
Alana Sales <129401794+Alana-Sales@users.noreply.github.com>
2025-04-23 19:49:01 -0300

    Create segundo_vetor.c

---
+++ segundo_vetor.c
@@ -0,0 +1,6 @@
+/*Declare um vetor de inteiro com no máximo 100 elementos;
+   • Pergunte ao usuários quantos elementos deseja digitar;
+   • Solicite ao usuário inserir os números;
+   • Pergunte qual posição do elemento que deseja remover;
+   • Remova o elemento;
+   • Imprima na tela o vetor resultante.*/
