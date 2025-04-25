ab88c66caf18ae062665a52c3b64ee9d503bfdf0
Alana Sales <129401794+Alana-Sales@users.noreply.github.com>
2025-04-25 11:54:25 -0300

    Update segundo_vetor.c

--- segundo_vetor.c
+++ segundo_vetor.c
@@ -1,9 +1,11 @@
-/*Declare um vetor de inteiro com no máximo 100 elementos;
+/*
+Declare um vetor de inteiro com no máximo 100 elementos;
    • Pergunte ao usuários quantos elementos deseja digitar;
    • Solicite ao usuário inserir os números;
    • Pergunte qual posição do elemento que deseja remover;
    • Remova o elemento;
-   • Imprima na tela o vetor resultante.*/
+   • Imprima na tela o vetor resultante.
+*/
 
 #include <stdio.h>
 
